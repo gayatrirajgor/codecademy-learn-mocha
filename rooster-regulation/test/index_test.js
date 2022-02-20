3182802398a58e7954d201adf5cb9b45a884679c
Gayatri R <gayatrirajgor@gmail.com>
2022-02-20 17:20:48 +0000

    added time at dawn test

--- rooster-regulation/test/index_test.js
+++ rooster-regulation/test/index_test.js
@@ -9,7 +9,7 @@ describe('Rooster', () => {
   describe('.announceDawn', () => {
     it('returns a rooster call', () => {
       //Setup
-      const expected = 'cock-a-doodle-doo'
+      const expected = 'cock-a-doodle-doo!'
 
       //Exercise
       const actual = Rooster.announceDawn()
@@ -18,18 +18,18 @@ describe('Rooster', () => {
       assert.equal(actual, expected)
     })
   })
+
+  describe('.timeAtDawn', () => {
+    it('returns its argument as a string', () => {
+      //Setup
+      const inputNumber = 12
+      const expected = '12'
+
+      //Exercise
+      const actual = Rooster.timeAtDawn(inputNumber)
+
+      //Verify
+      assert.equal(actual, expected)
+    })
+  })
 })
-
-// describe('Rooster', () => {
-//     describe('.announceDawn', () => {
-//         it('returns a rooster call', () => {
-
-//             //Setup
-//             const expected = 'cock-a-doodle-doo!';
-//             //Exercis
-//             const actual = Rooster.announceDawn()
-//             //Verify
-//             assert.equal(actual, expected)
-//         })
-//     })
-// })
